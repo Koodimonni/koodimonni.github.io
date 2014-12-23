b1c4aa1228821f04a8570bce476933995f7cc2ce
Onni Hakala <onni@koodimonni.fi>
2014-12-24 00:32:04 +0200

    changed cronjob to less verbose printing so cronjob mail would be more useful again

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@ set -e
 #Bash script for languages.koodimonni.fi cronjob
 
 #Ask for changes in translations from wordpress.org and save results in satis.json
-/usr/bin/php bin/wp-org-api >> /dev/null
+#Save into variable so that can be shouted out
+LOG=$(/usr/bin/php bin/wp-org-api)
 
 #Satis.json represents the state of known packages
 #When it changes there has been changes or additions in translations
@@ -13,6 +14,8 @@ CHANGED=$(git diff --name-only satis.json)
 if [ "$CHANGED" != "" ]
 then
   echo "Packages have changed!"
+  echo "LOG:"
+  echo $LOG
   echo "Updating changes into github..."
   NOW=$(date +"%T %m-%d-%y")
   #Build satis repos and push changes into github
